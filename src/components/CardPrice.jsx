0dce90563f0d86f5ebeef150b83953990865566e
Ariel Martinez <arielstereo@msn.com>
2024-05-13 13:03:18 -0300

    fix wsp button

--- src/components/CardPrice.jsx
+++ src/components/CardPrice.jsx
@@ -50,8 +50,8 @@ const CardPrice = ({
           </span>
         </li>
 
-        <li class="mb-4 flex">
-          <div class="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
+        <li className="mb-4 flex">
+          <div className="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -76,7 +76,7 @@ const CardPrice = ({
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span class="text-slate-950 font-semibold text-sm">
+          <span className="text-slate-950 font-semibold text-sm">
             Dominio incluido por 1 año
           </span>
         </li>

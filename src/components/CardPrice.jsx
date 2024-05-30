ecec13be7defde39373c010490c9ac4a4a370ed6
Ariel Martinez <arielstereo@msn.com>
2024-05-30 17:11:11 -0300

    change colors

--- src/components/CardPrice.jsx
+++ src/components/CardPrice.jsx
@@ -20,19 +20,19 @@ const CardPrice = ({
   return (
     <div
       data-aos={animation}
-      className="p-6 md:p-8 z-10 rounded-3xl shadow-lg shadow-black dark:shadow-slate-400 w-[340px] md:w-[450px] md:h-[620px] hover:bg-yellow-100 dark:bg-slate-100 dark:hover:bg-yellow-100"
+      className="p-6 md:p-8 z-10 rounded-3xl shadow-lg shadow-black dark:shadow-slate-200 w-[340px] md:w-[450px] md:h-[620px] bg-gradient-to-r from-blue-800 to-blue-600 hover:from-blue-600 hover:to-blue-800"
     >
-      <h3 className="mb-2 text-3xl md:text-4xl text-left">{title}</h3>
+      <h3 className="mb-2 text-3xl md:text-4xl text-left text-sky-200">{title}</h3>
       <div className="flex justify-start items-end">
-        <div className="text-4xl sm:text-5xl font-bold text-slate-950 text-left mt-4 mr-2">
+        <div className="text-4xl sm:text-5xl font-bold text-white text-left mt-4 mr-2">
           {price}/
         </div>
-        <div className="text-slate-600 font-semibold">Final</div>
+        <div className="text-slate-400 font-semibold">Final</div>
       </div>
-      <p className="mt-4 mb-6 text-blue-600 text-left text-sm md:text-lg">
+      <p className="mt-4 mb-6 text-slate-300 text-left text-sm md:text-lg">
         {subtitle}
       </p>
-      <ul className="mb-2 2xl:mb-6 text-black">
+      <ul className="mb-2 2xl:mb-6">
         <li className="mb-4 flex">
           <div className="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
             <svg
@@ -40,12 +40,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             {description}
           </span>
         </li>
@@ -57,12 +57,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">{check1}</span>
+          <span className="text-yellow-200 font-semibold text-sm">{check1}</span>
         </li>
         <li className="mb-4 flex">
           <div className="rounded-full bg-transparent w-5 h-5 flex justify-center items-center mr-4">
@@ -71,12 +71,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             Dominio incluido por 1 año
           </span>
         </li>
@@ -87,13 +87,13 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
-            Formulario de contacto
+          <span className="text-yellow-200 font-semibold text-sm">
+            Sección de contacto
           </span>
         </li>
         <li className="mb-4 flex">
@@ -103,12 +103,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             Carrousel de imágenes
           </span>
         </li>
@@ -119,12 +119,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             Botón flotante de WhatsApp
           </span>
         </li>
@@ -135,12 +135,12 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             Links a tus redes sociales
           </span>
         </li>
@@ -151,21 +151,21 @@ const CardPrice = ({
               viewBox="0 0 512 512"
               width="20px"
               height="20px"
-              className="fill-customSecondary"
+              className="fill-yellow-200"
             >
               <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path>
             </svg>
           </div>
-          <span className="text-slate-950 font-semibold text-sm">
+          <span className="text-yellow-200 font-semibold text-sm">
             Diseño para pc, tablets y celulares.
           </span>
         </li>
       </ul>
       <a
         href={href}
-        className="inline-block cursor-pointer text-center py-2 px-4 w-full rounded-xl rounded-t-xl bg-blue-600 hover:bg-white hover:text-blue-600 hover:border-2 hover:border-blue-600 text-white font-bold leading-loose mt-4 text-sm md:text-lg"
+        className="inline-block cursor-pointer text-slate-800 text-center py-2 px-4 w-full rounded-lg bg-white hover:bg-slate-800 hover:text-white border-2 border-white font-bold leading-loose mt-4 text-sm md:text-lg"
       >
-        Ver
+      Ver más
       </a>
     </div>
   );
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -63,7 +63,7 @@ const ContactForm = () => {
   };
 
   return (
-    <div data-aos="zoom-in-up" className="w-[330px] md:w-[400px] lg:w-fit mx-auto p-8 lg:p-12 mb-64 lg:mb-12 bg-slate-950 dark:bg-slate-100 shadow-md shadow-black rounded-lg">
+    <div data-aos="zoom-in-up" className="w-[330px] md:w-[400px] lg:w-fit mx-auto p-8 lg:p-12 mb-64 lg:mb-12 bg-gradient-to-r from-blue-800 to-blue-600 shadow-md shadow-black dark:shadow-white rounded-lg">
       <form
         onSubmit={sendEmail}
         className="flex flex-col gap-4 md:gap-8"
@@ -71,13 +71,13 @@ const ContactForm = () => {
         <div className="flex flex-col gap-4">
           <div>
             <label
-              className="block text-sm text-white dark:text-black font-medium mb-1"
+              className="block text-sm text-white font-medium mb-1"
               htmlFor="username"
             >
               Nombre
             </label>
             <input
-              className="w-full lg:w-96 p-2 rounded-md border-2 border-black"
+              className="w-full lg:w-96 p-2 rounded-md"
               id="username"
               name="username"
               placeholder="Juan Perez"
@@ -88,13 +88,13 @@ const ContactForm = () => {
           </div>
           <div>
             <label
-              className="block text-sm font-medium mb-1 text-white dark:text-black"
+              className="block text-sm font-medium mb-1 text-white"
               htmlFor="email"
             >
               Correo Electrónico
             </label>
             <input
-              className="w-full p-2 rounded-md border-2 border-black"
+              className="w-full p-2 rounded-md"
               id="email"
               name="email"
               placeholder="juan@gmail.com"
@@ -106,13 +106,13 @@ const ContactForm = () => {
           </div>
           <div>
             <label
-              className="block text-sm font-medium mb-1 text-white dark:text-black" 
+              className="block text-sm font-medium mb-1 text-white" 
               htmlFor="phone"
             >
               Teléfono
             </label>
             <input
-              className="w-full p-2 rounded-md border-2 border-black"
+              className="w-full p-2 rounded-md"
               id="phone"
               name="phone"
               placeholder="1122334455"
@@ -125,13 +125,13 @@ const ContactForm = () => {
         <div className="flex flex-col gap-6">
           <div>
             <label
-              className="block text-sm font-medium mb-1 text-white dark:text-black"
+              className="block text-sm font-medium mb-1 text-white"
               htmlFor="message"
             >
               Mensaje
             </label>
             <textarea
-              className="w-full lg:w-96 rounded-md p-4 h-32 border-2 border-black"
+              className="w-full lg:w-96 rounded-md p-4 h-32"
               id="msg"
               name="msg"
               placeholder="Escribe tu consulta"
@@ -142,7 +142,7 @@ const ContactForm = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-500 border-white dark:border-black border-2 p-2 text-white font-semibold rounded-md text-lg"
+            className="w-full bg-slate-100 hover:bg-slate-800 hover:text-white border-white border-2 p-2 text-black font-semibold rounded-md text-lg"
           >
             Enviar consulta
           </button>
